6284a812d62fe095dd48fc201a6912d791385363
lucianoinama <lucianoagustin.inama@gmail.com>
2023-06-03 00:46:59 -0300

    map

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,10 @@
-const map = L.map('mapa').setView([51.505, -0.09], 13);
+const map = L.map('mapa').setView([-34.542997465759115, -58.71187207895473], 16);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+
+var marker = L.marker([-34.542997465759115, -58.71187207895473]).addTo(map);
+
+marker.bindPopup("Plaza de SM").openPopup();
