3a3d49a2c508da852dddcdc70c7113d6c5c686eb
Francisco-pc <francisco.florio25@gmail.com>
2023-06-09 01:23:01 -0300

    Cambios generales

--- JS/Organizaciones.js
+++ JS/Organizaciones.js
@@ -10,9 +10,9 @@ function initMap() {
     L.latLng(-41, -64),
     L.latLng(-33, -56)
   );
-  
+
   map.setMaxBounds(bounds);
-  map.on('drag', function() {
+  map.on('drag', function () {
     map.panInsideBounds(bounds, { animate: false });
   });
 
@@ -47,11 +47,16 @@ function initMap() {
             let card = document.createElement('div');
             card.classList.add('card');
 
+            let Nombre = document.createElement('h1');
+            Nombre.textContent = 'Nombre: ' + organizacion.Nombre;
+            card.appendChild(Nombre);
+
             let descripcionElement = document.createElement('p');
             descripcionElement.textContent = 'Descripción: ' + organizacion.Descripción;
             card.appendChild(descripcionElement);
 
             let actividadesElement = document.createElement('ul');
+            actividadesElement.textContent = 'Actividades: ';
             actividadesElement.classList.add('actividades-list');
             organizacion.Actividades.forEach(actividad => {
               let actividadItem = document.createElement('li');
@@ -59,6 +64,24 @@ function initMap() {
               actividadesElement.appendChild(actividadItem);
             });
             card.appendChild(actividadesElement);
+
+            let serviciosElement = document.createElement('ul');
+            serviciosElement.textContent = 'Servicios: ';
+            serviciosElement.classList.add('servicios-list');
+            organizacion.Servicios.forEach(servicios => {
+              let serviciosItem = document.createElement('li');
+              serviciosItem.textContent = servicios;
+              serviciosElement.appendChild(serviciosItem);
+            });
+            card.appendChild(serviciosElement);
+
+            let horariosElement = document.createElement('p');
+            horariosElement.textContent = 'Horario de atencion: ' + organizacion.Horarios;
+            card.appendChild(horariosElement);
+
+            let telefonoElement = document.createElement('p');
+            telefonoElement.textContent = 'Telefono: ' + organizacion.Telefono;
+            card.appendChild(telefonoElement);
 
             let direccionElement = document.createElement('p');
             direccionElement.textContent = 'Dirección: ' + organizacion.Direccion;
@@ -69,29 +92,30 @@ function initMap() {
 
             // Utilizar las coordenadas para agregar un marcador al mapa
             normalizeAddress(organizacion.Direccion)
-            .then(direccionNormalizada => {
-          
-              var markerIcon = L.icon({
-                iconUrl: "/img/marcador/pngwing.com.png",
-                iconSize: [35, 35]
+              .then(direccionNormalizada => {
+
+                var markerIcon = L.icon({
+                  iconUrl: "/img/marcador/pngwing.com.png",
+                  iconSize: [35, 35]
+                });
+
+                var marker = L.marker(direccionNormalizada.coordenadas, { icon: markerIcon }).addTo(map);
+                nombreOrganizacion = organizacion.Nombre + ": "
+                marker.bindPopup( nombreOrganizacion.bold() + organizacion.Descripción);
+
+                // Hacer zoom en el marcador al hacer clic en él
+                marker.on('click', function () {
+                  map.flyTo(direccionNormalizada.coordenadas, 15);
+                });
+
+                // Hacer zoom en el marcador al hacer clic en la tarjeta
+                card.addEventListener('click', function () {
+                  map.flyTo(direccionNormalizada.coordenadas, 15);
+                });
+              })
+              .catch(error => {
+                console.error('Error al normalizar la dirección:', error);
               });
-          
-              var marker = L.marker(direccionNormalizada.coordenadas, { icon: markerIcon}).addTo(map);
-              marker.bindPopup(organizacion.Descripción);
-          
-              // Hacer zoom en el marcador al hacer clic en él
-              marker.on('click', function() {
-                map.flyTo(direccionNormalizada.coordenadas, 15);
-              });
-          
-              // Hacer zoom en el marcador al hacer clic en la tarjeta
-              card.addEventListener('click', function() {
-                map.flyTo(direccionNormalizada.coordenadas, 15);
-              });
-            })
-            .catch(error => {
-              console.error('Error al normalizar la dirección:', error);
-            });
           }
         });
       }
@@ -99,14 +123,13 @@ function initMap() {
       renderCards();
 
       // Buscar organizaciones por actividad
-      searchInput.addEventListener('input', function() {
-        filteredOrganizaciones = organizaciones.filter(organizacion => {
+      searchInput.addEventListener('input', function () {
+        filteredOrganizacionesActividad = organizaciones.filter(organizacion => {
           let actividades = organizacion.Actividades;
           let searchTerm = searchInput.value.toLowerCase();
 
           return actividades.some(actividad => actividad.toLowerCase().includes(searchTerm));
         });
-
         renderCards();
       });
     })
@@ -114,6 +137,7 @@ function initMap() {
       console.error('Error al cargar el archivo JSON:', error);
     });
 }
+
 
 // Función para normalizar una dirección y obtener las coordenadas
 function normalizeAddress(direccion) {
